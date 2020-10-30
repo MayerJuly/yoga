1a49cbdf800f29858a8c1febaf65949eeba0c26e
MayerJuly <73027797+MayerJuly@users.noreply.github.com>
2020-10-30 11:30:16 +0300

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,50 @@
+// ANIMATION TEXT
+
+const animItems = document.querySelectorAll('._anim')
+
+if(animItems.length>0) {
+    window.addEventListener('scroll', animOnScroll)
+
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffset = offset(animItem)
+            const animStart = 4
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active')
+            } else {
+                if (!animItem.classList.contains('_anim-no-hide'))
+                    animItem.classList.remove('_active')
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return (rect.top + scrollTop)
+
+    }
+
+    setTimeout(() => {
+        animOnScroll()
+    }, 200)
+
+    window.addEventListener('resize', checkNav)
+
+    function checkNav() {
+        if (document.documentElement.clientWidth < 860) {
+            const header = document.getElementById('header')
+            header.classList.remove('_anim')
+        }
+    }
+    checkNav()
+}
---
+++ js/app.js
@@ -0,0 +1,158 @@
+$(function() {
+
+    let header = $('#header')
+    let intro = $('#intro')
+    let nav = $('.nav')
+
+    let introH = intro.innerHeight()
+    let scrollPos = $(window).scrollTop()
+
+    checkScroll(scrollPos,introH)
+
+    $(window).on('scroll resize ', () => {
+        introH = intro.innerHeight()
+        scrollPos = $(this).scrollTop()
+        checkScroll(scrollPos,introH)
+    })
+
+    function checkScroll(scrollPos, introH) {
+        if(scrollPos > introH){
+            header.addClass('fixed')
+        } else{
+            header.removeClass('fixed')
+        }
+    }
+
+    // SLIDER
+
+    let slider = $('#lessonsSlider')
+
+    slider.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        fade: false,
+        arrows: true,
+        dots: true,
+
+    });
+
+
+
+
+
+    //     Smooth scroll
+    $("[data-scroll]").on('click', function(event) {
+        event.preventDefault()
+
+        let elementID = $(this).data('scroll')
+        let elementOffset = $(elementID).offset().top
+
+        nav.removeClass('active')
+        $('.ham').removeClass('active')
+
+
+        if (elementID === '#features')
+            $('html, body').animate({
+                scrollTop: elementOffset + 65,
+            }, 700)
+        else {
+            $('html, body').animate({
+                scrollTop: elementOffset - 75,
+            }, 700)
+        }
+    })
+
+
+//    Modal
+
+    const close = $('.js-close-popup')
+    const modalOverlay = $('.overlay')
+    const popup = $('.js-popup')
+
+
+    function closeModal(close,object) {
+        close.click(function (){
+            object.removeClass('active')
+        })
+    }
+
+
+    function closeModalOverlay(popup,object){
+        $(document).mousedown(function (e) {
+            if (e.target !== popup[0] && popup.has(e.target).length === 0) {
+               object.removeClass('active')
+            }
+        })
+    }
+
+    $('.js-button-popup').click(function (){
+        modalOverlay.addClass('active')
+    })
+
+    closeModal(close,modalOverlay)
+
+    closeModalOverlay(popup,modalOverlay)
+
+
+    //    Modal PLAY
+
+    const modalPlayOverlay = $('.overlay-play')
+    const popupPlay = $('.js-popup-play')
+
+
+
+    $('.js-button-popup-play').click(function (){
+        modalPlayOverlay.addClass('active')
+    })
+
+    closeModal(close,modalPlayOverlay)
+
+    closeModalOverlay(popupPlay,modalPlayOverlay)
+
+
+
+//    BURGER
+
+
+    const burger = $('.burger')
+
+    burger.on('click', function () {
+        nav.toggleClass('active')
+        })
+
+
+
+})
+
+
+// Arrow's-up
+
+let userH = $(window).height()
+let scrollPos = $(window).scrollTop()
+let arrow = $('#arrow')
+arrow.on('click', () => {
+
+    $('html, body').animate( {
+        scrollTop: 0
+    },700)
+})
+
+
+
+//    Header fixed
+checkScroll(scrollPos, userH)
+
+$(window).on('scroll resize', () => {
+    scrollPos = $(this).scrollTop()
+    checkScroll(scrollPos, userH)
+})
+
+function checkScroll(scrollPos, userH) {
+    if(scrollPos > userH) {
+        arrow.addClass('active')
+    } else{
+        arrow.removeClass('active')
+    }
+}
