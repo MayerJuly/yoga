4dcd0bcc61c3dfc5f69d54344f8637d02f5655bf
MayerJuly <73027797+MayerJuly@users.noreply.github.com>
2020-11-01 16:21:50 +0300

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -1,50 +1,60 @@
-// ANIMATION TEXT
 
-const animItems = document.querySelectorAll('._anim')
 
-if(animItems.length>0) {
-    window.addEventListener('scroll', animOnScroll)
+document.body.onload= function() {
+    let preload = document.getElementById('pagePreloader')
+    setTimeout(function () {
+        if (!preload.classList.contains('done')){
+            preload.classList.add('done')
+            const animItems = document.querySelectorAll('._anim')
 
-    function animOnScroll() {
-        for (let index = 0; index < animItems.length; index++) {
-            const animItem = animItems[index]
-            const animItemHeight = animItem.offsetHeight
-            const animItemOffset = offset(animItem)
-            const animStart = 4
+            if (animItems.length > 0) {
+                window.addEventListener('scroll', animOnScroll)
 
-            let animItemPoint = window.innerHeight - animItemHeight / animStart
+                function animOnScroll() {
+                    for (let index = 0; index < animItems.length; index++) {
+                        const animItem = animItems[index]
+                        const animItemHeight = animItem.offsetHeight
+                        const animItemOffset = offset(animItem)
+                        const animStart = 4
 
-            if (animItemHeight > window.innerHeight) {
-                animItemPoint = window.innerHeight - window.innerHeight / animStart
-            }
+                        let animItemPoint = window.innerHeight - animItemHeight / animStart
 
-            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-                animItem.classList.add('_active')
-            } else {
-                if (!animItem.classList.contains('_anim-no-hide'))
-                    animItem.classList.remove('_active')
+                        if (animItemHeight > window.innerHeight) {
+                            animItemPoint = window.innerHeight - window.innerHeight / animStart
+                        }
+
+                        if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                            animItem.classList.add('_active')
+                        } else {
+                            if (!animItem.classList.contains('_anim-no-hide'))
+                                animItem.classList.remove('_active')
+                        }
+                    }
+                }
+
+                function offset(el) {
+                    const rect = el.getBoundingClientRect(),
+                        scrollTop = window.pageYOffset || document.documentElement.scrollTop
+                    return (rect.top + scrollTop)
+
+                }
+
+                setTimeout(() => {
+                    animOnScroll()
+                }, 200)
+
+                window.addEventListener('resize', checkNav)
+
+                function checkNav() {
+                    if (document.documentElement.clientWidth < 860) {
+                        const header = document.getElementById('header')
+                        header.classList.remove('_anim')
+                    }
+                }
+
+                checkNav()
             }
         }
-    }
 
-    function offset(el) {
-        const rect = el.getBoundingClientRect(),
-            scrollTop = window.pageYOffset || document.documentElement.scrollTop
-        return (rect.top + scrollTop)
-
-    }
-
-    setTimeout(() => {
-        animOnScroll()
-    }, 200)
-
-    window.addEventListener('resize', checkNav)
-
-    function checkNav() {
-        if (document.documentElement.clientWidth < 860) {
-            const header = document.getElementById('header')
-            header.classList.remove('_anim')
-        }
-    }
-    checkNav()
+    }, 1000)
 }
